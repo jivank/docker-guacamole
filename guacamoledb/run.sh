0750afe38f22538bb9b74e309209376c79224399
Jivan Kulkarni <jivan@fa-eng.com>
2015-05-06 11:39:15 -0700

    full paths in run.sh

--- guacamoledb/run.sh
+++ guacamoledb/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gosu postgres pg_ctl -w start
 
-gosu postgres psql -h localhost -p 5432 -U postgres -a -f createdb.sql
-gosu postgres psql -h localhost -p 5432 -U postgres -d guacamole_db -a -f initdb.sql
+gosu postgres psql -h localhost -p 5432 -U postgres -a -f /docker-entrypoint-initdb.d/createdb.sql
+gosu postgres psql -h localhost -p 5432 -U postgres -d guacamole_db -a -f /docker-entrypoint-initdb.d/initdb.sql
 
 gosu postgres pg_ctl stop
